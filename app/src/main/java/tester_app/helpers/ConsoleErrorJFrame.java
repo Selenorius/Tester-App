316eec287dcb36bd081920beaf41b3d62bb55e4a
JP <jo.percht@gmail.com>
2026-07-29 13:03:02 +0200

    small change

--- app/src/main/java/tester_app/helpers/ConsoleErrorJFrame.java
+++ app/src/main/java/tester_app/helpers/ConsoleErrorJFrame.java
@@ -8,8 +8,9 @@ import static tester_app.helpers.Constants.tab;
 import javax.swing.JFrame;
 
 public abstract class ConsoleErrorJFrame extends JFrame implements ConsoleErrorMessage {
-   public void consoleErrorMessage(String message) {
-        System.out.println(ANSI_PURPLE + this.getClass().getName());
+    @Override
+    public void consoleErrorMessage(String message) {
+        System.out.println(ANSI_PURPLE + this.getClass().getName().substring(11));
         System.out.print(ANSI_RED + tab());
         System.out.println(message);
         System.out.println(ANSI_RESET);
